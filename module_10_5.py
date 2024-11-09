5b5a2b0ecd1111ea2f1ad45c93e0bbe556718876
ПК <@DESKTOP-H9D4HFD>
2024-11-09 22:15:37 +0700

    Добавьте файлы проекта.

---
+++ module_10_5.py
@@ -0,0 +1,26 @@
+﻿import datetime
+import multiprocessing
+
+filenames = ['file 1.txt', 'file 2.txt', 'file 3.txt', 'file 4.txt']
+
+def read_info(name):
+    all_data = []
+    with open(name) as file_:
+        while True:
+            data = file_.readline()
+            all_data.append(data)
+            if not data:
+                break
+
+start = datetime.datetime.now()
+for file in filenames:
+    read_info(file)
+end = datetime.datetime.now()
+print(f'{end - start} (линейный)')
+
+# if __name__ == '__main__':
+#     start = datetime.datetime.now()
+#     with multiprocessing.Pool(processes=len(filenames)) as pool:
+#         pool.map(read_info, filenames)
+#     end = datetime.datetime.now()
+#     print(f'{end - start} (многопроцессорный)')
